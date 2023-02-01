7e2243653514a23d2266fd69c8638982fc87c143
Rshefa <raisas@nycstudents.net>
2023-02-01 10:57:53 -0500

    done

---
+++ src/Runner.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        numList.add(10);
+        numList.add(5);
+        numList.add(24);
+        System.out.println(numList);
+    }
+
+    }
